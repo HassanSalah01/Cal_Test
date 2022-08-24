1400b6b55fbba1f3f962cf943c28fa5f3d2a1d86
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-08-24 22:00:57 +0200

    updated_app.js

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 let add = "";
 const liList = document.querySelectorAll("li");
 const newNumb = document.getElementById("new");
+const oldNumb = document.getElementById("old");
+let locStor = "";
 for (let i of liList){
-    i.addEventListener('click',()=>{
-        add+=i.innerText;
-        newNumb.innerHTML=add;
-    })
+    i.style.cursor = "default"
+    // console.log(i.innerHTML,!isNaN(parseInt(i.innerHTML)));
+    if(i.innerHTML,!isNaN(parseInt(i.innerHTML))){
+        i.addEventListener("click",()=>{
+            console.log(i.innerHTML);
+            locStor = i.innerHTML;
+            add+=i.innerHTML;
+            newNumb.innerHTML = add;
+            console.log(locStor);
+        })
+    }else{
+        i.addEventListener("click",()=>{
+            locStor=i.innerHTML;
+            console.log(locStor);
+        })
+    }
 }
+
+// if(add.length > 0 && )
